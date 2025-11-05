8915cb608e84f25e21ed30c79676daa0a944645e
SiddarthaBeemaneni <siddarthabeemaneni@gmail.com>
2025-11-05 23:12:01 +0530

    Add files via upload

---
+++ 9.2.py
@@ -0,0 +1,11 @@
+try:
+    age = int(input("Enter your age: "))
+
+    if age < 0:
+        raise ValueError("Age cannot be negative!")
+    elif age < 18:
+        print("❌ You are not eligible to vote.")
+    else:
+        print("✅ You are eligible to vote.")
+except ValueError as e:
+    print("Invalid input:", e)
